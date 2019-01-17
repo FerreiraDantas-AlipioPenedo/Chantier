3fe0abc0102aba04bf99972ae4a88cead54df314
Joao Paulo Alipio Penedo <joao-paulo.ALIPIO-PENEDO@cpnv.ch>
2019-01-17 10:21:40 +0100

    màj

--- Chantier/UnitTestProject1/UnitTest1.cs
+++ Chantier/UnitTestProject1/UnitTest1.cs
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
-=======
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
->>>>>>> 5b566b1ff780d2ed2fd1b91bd3aef8f45a3112a2
 using ClassLibrary;
 
 namespace UnitTestChantier
@@ -17,11 +12,8 @@ namespace UnitTestChantier
         [TestMethod]
         public void TestMethodAddVehicle()
         {
-<<<<<<< HEAD
             List<Vehicles> vehicles = new List<Vehicles>();
-=======
-            
->>>>>>> 5b566b1ff780d2ed2fd1b91bd3aef8f45a3112a2
+            Vehicles v = new Vehicles(2, 4, 250, 250);
         }
     }
 }
