6ba9a900a29148846cc8f65b843a46f96ac6bd4d
Joao Paulo Alipio Penedo <joao-paulo.ALIPIO-PENEDO@cpnv.ch>
2019-01-17 09:20:23 +0100

    Màj Projet

---
+++ Chantier/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestChantier
+{
+    [TestClass]
+    public class UnitTestChantier
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
