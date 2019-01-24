cf55cea27042e4d5651d331e4a0a4fc270794851
Filipe Ferreira Dantas <Filipe.FERREIRA-DANTAS@cpnv.ch>
2019-01-24 10:04:51 +0100

    Résolution de bug

--- Chantier/Chantier/Properties/AssemblyInfo.cs
+++ Chantier/Chantier/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("b851b13e-80b6-4e41-986d-d747f799c039")]
+[assembly: Guid("bab8edfd-e4bf-45fb-915a-2970ce9ba051")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
---
+++ Chantier/UnitTestChantier/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chantier
+{
+    [TestClass]
+    public class UnitTestChantier
+    {
+        [TestMethod]
+        public void TestMethodAddVehicle()
+        {
+            int expectedVehiclesInList = 2;
+
+            List<Vehicles> vehicles = new List<Vehicles>();
+            Vehicles v = new Vehicles(2, 4, 250, 250);
+            Truck t = new Truck(2, 6, 500, 1000, true);
+
+            vehicles.Add(t);
+            vehicles.Add(v);
+            
+
+            Assert.AreEqual(expectedVehiclesInList, vehicles.Count);
+        }
+
+        [TestMethod]
+        public void TestMethodVehiclesInSite()
+        {
+           
+
+
+            //Assert.AreEqual(expectedVehiclesInList, vehicles.Count);
+        }
+    }
+}
